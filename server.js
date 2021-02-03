affcf98ae70108e836169bf4ec3a6c34545dac68
Logan Zehr <zehrl315@outlook.com>
2021-02-03 13:53:23 -0800

    replaced var with const in server.js file. Added dotenv dependency to try to fix process.env.PORT issues

--- server.js
+++ server.js
@@ -2,14 +2,15 @@
 // =============================================================
 const express = require("express")
 const nodemon = require("nodemon")
+const dotenv = require('dotenv').config();
 
 const path = require('path')
 const fs = require("fs")
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
